77056792393453d4504487c231e2002775605bde
Igor Terekhin <terekhin01@mail.ru>
2021-01-29 13:51:14 +0500

    add scheduler

--- src/__init__.py
+++ src/__init__.py
@@ -2,12 +2,12 @@
 from .TTASupervisedRunner import TTASupervisedRunner as Runner
 from .experiment import Experiment
 from .TTASupervisedRunner import TTASupervisedRunner
-from .ResNextFacebook32x8d import ResNextFacebook32x8d
 from .resnet18 import ResNet18
 from catalyst.dl import registry
 from catalyst.contrib.callbacks.confusion_matrix_logger import ConfusionMatrixCallback
 
 from callbacks.iner_callback import InerCallback
+from callbacks.new_scheduler import CustomScheduler
+
 registry.Model(ResNet18)
 registry.Callback(ConfusionMatrixCallback)
-registry.Model(ResNextFacebook32x8d)
