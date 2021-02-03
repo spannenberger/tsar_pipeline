5ca558478634795dbe560ba6a3f84d511961ad37
Igor Terekhin <63662226+spannenberger@users.noreply.github.com>
2021-02-03 18:04:41 +0500

    Update train_kfold.sh

--- train_kfold.sh
+++ train_kfold.sh
@@ -11,8 +11,7 @@ then
                         --stages/data_params/num_folds=$(($1)):int \
                         --stages/data_params/fold_index=$(($i - 1)):int \
                         --stages/callbacks_params/infer/fold_index=$(($i - 1)):int \
-                        --stages/callbacks_params/infer/num_folds=$(($1)):int \
-                        --check
+                        --stages/callbacks_params/infer/num_folds=$(($1)):int
         python fold_metric.py $i
         echo "================\n"
         done
