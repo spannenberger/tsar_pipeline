eaa258b6b145dd3e32a6387e9b90ec22a70b89a0
Igor Terekhin <terekhin01@mail.ru>
2021-02-10 11:52:43 +0500

    fix iner_callback & augs

--- src/callbacks/iner_callback.py
+++ src/callbacks/iner_callback.py
@@ -2,6 +2,9 @@ from catalyst.dl import registry, Callback, CallbackOrder, State
 from sklearn.metrics import accuracy_score
 import pandas as pd
 from pathlib import Path
+from sklearn.metrics import log_loss
+from torch.nn import CrossEntropyLoss
+
 
 @registry.Callback
 class InerCallback(Callback):
@@ -14,6 +17,8 @@ class InerCallback(Callback):
         self.subm_file.parent.mkdir(parents=True, exist_ok=True)
         self.final = []
         self.ac = -1
+        self.loss_f = CrossEntropyLoss(reduce=False)
+
 
     def on_batch_end(self, state: State):
         if state.is_valid_loader:
@@ -21,10 +26,13 @@ class InerCallback(Callback):
             self.targets += state.input['targets'].tolist()
             preds = state.output["logits"].detach().cpu().numpy()
             self.preds += list(preds.argmax(axis=1))
+            self.losses += list(self.loss_f(state.output["logits"].detach().cpu(),state.input['targets'].detach().cpu()))
+            
     def on_loader_start(self, _):
         self.paths = []
         self.targets = []
         self.preds = []
+        self.losses = []
 
     def on_loader_end(self, state: State):
         if state.is_valid_loader:
@@ -32,23 +40,23 @@ class InerCallback(Callback):
             if cur_ac > self.ac:
                 self.ac = cur_ac
                 self.final = []
-                for path, pred, target in zip(self.paths, self.preds, self.targets):
-                    self.final.append((path, pred, target))
+                for path, pred, target, loss in zip(self.paths, self.preds, self.targets, self.losses):
+                    self.final.append((path, pred, target, loss))
 
 
     def on_experiment_end(self, _):
         if self.num_folds == 0:
-            subm = ["path,class_id,target"]
-            subm += [f"{path},{cls},{tar}" for path, cls, tar in self.final]
+            subm = ["path,class_id,target,losses"]
+            subm += [f"{path},{cls},{tar},{los}" for path, cls, tar, los in self.final]
             with self.subm_file.open(mode = 'w') as file:
                 file.write("\n".join(subm)+"\n")
         else:
             if self.fold_index == 0:
-                subm = ["path,class_id,target"]
-                subm += [f"{path},{cls},{tar}" for path, cls, tar in self.final]
+                subm = ["path,class_id,target,losses"]
+                subm += [f"{path},{cls},{tar},{los}" for path, cls, tar, los in self.final]
                 with self.subm_file.open(mode = 'w') as file:
                     file.write("\n".join(subm)+"\n")
             else:
-                subm = [f"{path},{cls},{tar}" for path, cls, tar in self.final]
+                subm = [f"{path},{cls},{tar},{los}" for path, cls, tar, los in self.final]
                 with self.subm_file.open(mode = 'a') as file:
                     file.write("\n".join(subm)+"\n")
