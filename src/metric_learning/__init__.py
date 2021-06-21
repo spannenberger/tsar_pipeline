98f08ddd525636e6dcf8a8cfb86af5862035b3e6
Igor <i.terekhin@napoleonit.ru>
2021-06-21 14:59:26 +0500

    fix loss, test models

--- src/metric_learning/__init__.py
+++ src/metric_learning/__init__.py
@@ -1,16 +1,17 @@
 from catalyst.registry import Registry
 from SupervisedRunner import MertricLearningSupervisedRunner
+
 from models.ResNet18 import ResNet18
 from models.ResNext50 import ResNext50
 from models.MobilenetV3Small import MobilenetV3Small
-from models.EfficientNetB0 import EfficientNetB0
+
 from callbacks.criterion import CustomTrainCriterion
 from callbacks.cmc_valid import CustomCMC
 from callbacks.custom_scheduler import CustomScheduler
+
 Registry(ResNext50)
 Registry(ResNet18)
 Registry(MobilenetV3Small)
-Registry(EfficientNetB0)
 Registry(CustomTrainCriterion)
 Registry(CustomCMC)
 Registry(MertricLearningSupervisedRunner)
--- src/metric_learning/callbacks/criterion.py
+++ src/metric_learning/callbacks/criterion.py
@@ -4,14 +4,15 @@ from .archface_loss import AngularPenaltySMLoss
 
 
 class CustomCriterion(CriterionCallback):
-    def __init__(self, embeding_size, num_classes, *args, **kwargs):
+    def __init__(self, embeding_size, num_classes, loss_type='arcface', *args, **kwargs):
+        self.loss_type = loss_type
         self.embeding_size = embeding_size
         self.num_classes = num_classes
         super().__init__(*args, **kwargs)
 
     def on_stage_start(self, runner):
         self.criterion = AngularPenaltySMLoss(
-            self.embeding_size, self.num_classes, loss_type='arcface')
+            self.embeding_size, self.num_classes, loss_type=self.loss_type)
 
 
 class CustomTrainCriterion(dl.ControlFlowCallback):
